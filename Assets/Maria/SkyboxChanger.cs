8848e475a3f22e75c259ae74f1cf70917398aa3f
mqgca-iscte <mqgca@iscte-iul.pt>
2024-06-20 18:38:21 +0100

    LevelsDescription

---
+++ Assets/Maria/SkyboxChanger.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+
+public class SkyboxChanger : MonoBehaviour
+{
+    public Material[] skyboxes; // Array to hold the skybox materials
+    public float changeInterval = 100f; // Interval time in seconds
+    public GameObject[] seasons;
+    public GameObject panel;
+
+    private int currentSkyboxIndex = 0;
+    private float timer;
+
+    void Start()
+    {
+        if (skyboxes.Length > 0)
+        {
+            RenderSettings.skybox = skyboxes[currentSkyboxIndex];
+        }
+        timer = changeInterval;
+        ChangeSkybox();
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            ChangeSkybox();
+            timer = changeInterval;
+        }
+    }
+
+     IEnumerator WaitAndChangeSkybox()
+    {
+        yield return new WaitForSeconds(6f);
+        panel.gameObject.SetActive(false);
+        seasons[currentSkyboxIndex].SetActive(false);
+    }
+
+    void ChangeSkybox()
+    {
+        int old = currentSkyboxIndex;
+        if (currentSkyboxIndex < skyboxes.Length - 1){
+        currentSkyboxIndex = currentSkyboxIndex + 1;
+        RenderSettings.skybox = skyboxes[currentSkyboxIndex];
+        panel.gameObject.SetActive(true);
+        seasons[currentSkyboxIndex].SetActive(true);
+        seasons[old].SetActive(false);
+        StartCoroutine(WaitAndChangeSkybox());
+        }
+    }
+    
+}
