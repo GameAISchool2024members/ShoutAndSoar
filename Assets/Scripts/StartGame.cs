5712564ae80417a1a37e9a292eb439d503366170
ajpessoa <rvblack90@gmail.com>
2024-06-21 14:07:02 +0200

    Add main menu scene

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitApplication()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
+
