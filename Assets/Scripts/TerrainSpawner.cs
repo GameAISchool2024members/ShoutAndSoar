cf69ec973cbcfea4c3e3b9d6ddc0c43662d6d9a6
Alireza Fatemi <seyedalirezafatemijahromi@gmail.com>
2024-06-21 15:56:50 +0300

    minor fixes

--- Assets/Scripts/TerrainSpawner.cs
+++ Assets/Scripts/TerrainSpawner.cs
@@ -48,6 +48,8 @@ public class TerrainSpawner : MonoBehaviour
         {
             Destroy(plane);
         }
+        // Empty the list
+        _planes.Clear();
         for (int i = 0; i < _initialSpawn; i++)
         {
             SpawnPlanes();
