047c4e0cc367a6a018ecd0abddd43a61a8e6e071
Aleksander <aleksander@fjellvang.dk>
2024-06-21 09:08:37 +0200

    introduced InputController

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,60 @@
+using Mediapipe.Unity;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField] private PoseLandmarkListAnnotation listAnnotation;
+    [SerializeField] private float deltaThreshold = 0.5f;
+
+    public Vector2 GetInput()
+    {
+        var input = GetInputFromPose();
+        return CalculateMovement(input.x, input.y); 
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        var input = GetInputFromPose();
+        var movement = CalculateMovement(input.x, input.y);
+    }
+
+    private Vector2 GetInputFromPose()
+    {
+        var lefthand = listAnnotation[15];
+        var leftshoulder = listAnnotation[11];
+        
+        var righthand = listAnnotation[16];
+        var rightshoulder = listAnnotation[12];
+
+        float leftdelta = lefthand.transform.position.y - leftshoulder.transform.position.y;
+        float rightdelta = righthand.transform.position.y - rightshoulder.transform.position.y;
+        return new Vector2(leftdelta, rightdelta);
+    }
+
+    private Vector2 CalculateMovement(float leftdelta, float rightdelta)
+    {
+        var movingUp = leftdelta> 0 && rightdelta > 0;
+        var movingDown = leftdelta < 0 && rightdelta < 0;
+        var movingLeft = leftdelta < 0 && rightdelta > 0;
+        var movingRight = leftdelta > 0 && rightdelta < 0;
+        
+        if(movingUp){
+            return new Vector2(0, 1);
+        }
+
+        if(movingDown)
+        {
+            return new Vector2(0, -1);
+        }
+
+        if(movingLeft){
+            return new Vector2(-1, 0);
+        }
+
+        if(movingRight){
+            return new Vector2(1, 0);
+        }
+        return Vector2.zero;
+    }
+}
