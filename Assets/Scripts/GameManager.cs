e95f48e78d0efc9ffd2796c650933946c7ff8e5f
Aleksander <aleksander@fjellvang.dk>
2024-06-21 15:01:01 +0200

    gameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,9 +10,12 @@ public class GameManager : MonoBehaviour
     private PointManager _pointManager;
     [SerializeField]
     private SkyboxChanger _skyboxChanger;
-
     [SerializeField] private float _gameTime = 60f;
     private float _gameTimer;
+    
+    [SerializeField]
+    Image GameOverPanel;
+    
     private void Start()
     {
         _pointManager.ResetPoints();
@@ -21,6 +27,17 @@ public class GameManager : MonoBehaviour
         _gameTimer += Time.deltaTime;
         if (_gameTimer >= _gameTime)
         {
+            StartCoroutine(FadeInEnumerator());
         }
+    }
+
+    IEnumerator FadeInEnumerator()
+    {
+        while (GameOverPanel.color.a < 1)
+        {
+            GameOverPanel.color = new Color(GameOverPanel.color.r, GameOverPanel.color.g, GameOverPanel.color.b, GameOverPanel.color.a + Time.deltaTime);
+            yield return null;
+        }
+        // SceneManager.LoadScene()
     }
 }
